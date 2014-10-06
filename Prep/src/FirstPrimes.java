b478f7b43696e44fdb5c19f962097ebbbda529d3
Jaclyn Foss <andrew.n.chalmers@gmail.com>
2014-10-06 00:40:03 -0400

    Fixed

--- Prep/src/FirstPrimes.java
+++ Prep/src/FirstPrimes.java
@@ -1,7 +1,12 @@
 
+
 public class FirstPrimes {
 
 	// Write a method to return the first N prime integers
+    
+    
+    
+    // Methods to test algorithm ----------------->
 	
 	public static String testArraysEqual(int[] primes, int[] result, String testName) {
 		  boolean passed = true; 
@@ -14,6 +19,15 @@ public class FirstPrimes {
 		  if (passed) return testName + " passed\n"; 
 		  else return testName + " failed\n";
 	}
+    
+    public static void findFirstMillionPrimes() {
+        long startTime = System.nanoTime();
+        int[] primeList = getPrimes(1000000);
+        System.out.println(String.valueOf(primeList[primeList.length-1]));
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println(String.valueOf(duration) + " nanoseconds, " + String.valueOf(duration/1000) + " milliseconds");
+    }
 	
 	public static String testPrimes() {
 	  String testResults = "First Primes: \n";
@@ -37,31 +51,68 @@ public class FirstPrimes {
 	  result = getPrimes(-1);
 	  if (result != null) testResults += "test 4 failed \n";
 	  else testResults += "test 4 passed \n";
-
+	  
+        
 	  return testResults;
 	}
+    
+    // <--------------- end of test methods
+    
+    
+    
+    // Method to return first N primes -------->
 
 	public static int[] getPrimes(int n) {
+        
+      // return if n is too small for safe error handling
 	  if (n <= 0) return null;
+     
+      // create an array of size N
 	  int[] primes = new int[n];
+    
+      // count number of inserted primes
 	  int insertions = 0;
+        
+      // j is the number that may be prime (so we start at 2)
 	  int j = 2;
+        
+      // until N primes are found, loop. (each loop increments j)
 	  while (insertions < n) {
+          
+        // create an index to loop through existing primes in array of primes
 	    int i = 0;
+          
+        // set a flag; if this flag isn't changed after the loop the number is prime
 	    boolean isPrime = true;
+          
+        // if a number is not prime or a square it will have two integer factors.
+        // check only the previous primes that are less than or equal to the sqrt
+        // of the current number K, because if a number is not prime or a square
+        // it will have two integer factors (one greater, one less, than K)
+		int sqrtFloor = ((int) Math.sqrt(j));
+        
+        // start looping through all found primes
 	    while (i < insertions) {
+            
+          // break if primes[i] is greater than sqrtFloor
+	      if (primes[i] > sqrtFloor) break;
+            
+          // if integer j mod primes[i] is 0 then primes[i] is a factor of j, so break
 	      if ((j % primes[i]) == 0) {
 	        isPrime = false;
 	        break;
 	      }
 	      i++; 
 	    }
+        
+        // if the flag is set, add the current integer j to the set of primes
 	    if (isPrime) {
 	      primes[insertions] = j;
 	      insertions++;
 	    }
 	    j++;
 	  }
+        
 	  return primes ;
 	}
 }
